14be04f342b8269cdcbe070b0555248fc94a7261
UchihaBenji <51889644+UchihaBenji@users.noreply.github.com>
2019-06-16 15:28:59 -0500

    Add files via upload

---
+++ calculadoraniveldios.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+
+int main() {
+    float x, y, resultado;
+    char op;
+    int valido = 1;
+    
+    printf("Esta es una calculadora simple, para sumar ingrese +, para restar -, para dividir /, para multiplicar * o x\n");
+    printf("Ingrese operacion: ");
+    scanf("%c", &op);
+    printf("Ingrese x: ");
+    scanf("%f", &x);
+    printf("Ingrese y: ");
+    scanf("%f", &y);
+
+    switch (op) {
+        case '+':
+            resultado = x + y;
+            break;
+        case '-':
+            resultado = x - y;
+            break;
+        case '*':
+        case 'x':
+            resultado = x * y;
+            break;
+        case '/':
+            resultado = x / y;
+            break;
+        default:
+            valido = 0;
+    }
+
+    if (valido)
+        printf("El resultado es %f\n", resultado);
+    else
+        printf("Operacion invalida\n")
+
+    return 0;
+}
